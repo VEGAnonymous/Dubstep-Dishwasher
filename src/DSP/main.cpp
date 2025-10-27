e060ec3962d86adba82d5c9de4e06e804889783b
VEGAnonymous <lyreponymous@protonmail.com>
2025-10-26 19:03:38 -0700

    chore: add command struct to ESP32

--- src/DSP/main.cpp
+++ src/DSP/main.cpp
@@ -14,6 +14,8 @@
 #include "Control.h"
 #include "LUTs.h"
 
+#define RX_PIN 0
+
 // TESTING - 
 const bool USB_IO = true, // <-- SET FLAGS HERE
            LOG_DBG = true;
@@ -34,29 +36,45 @@ std::unique_ptr<AudioChainStream> stream;
 // Connections
 std::unique_ptr<AudioConnection> patch1, patch2;
 
-void processCommand(const String& s, AudioChain& chain) {
-    uint8_t fxID, paramID, bypass;
-    float value;
+// Command structure
+struct Command {
+    uint8_t cmd;        // Command type (0 = Add, 1 = Remove, 2 = Reorder, 3 = Set param, 4 = Bypass)
+    uint8_t id1;        // Effect id 1
+    uint8_t id2;        // Effect id 2 or ParamID
+    uint8_t reserved;   // Reserved/padding
+    float value;        // Parameter value (float)
+} __attribute__((packed));
 
-    // Command structure: [Type][EffectID][ParamID/Bypass][Value], ex:
-    // S 1 2 0.75
-    // B 1 1
-    if (sscanf(s.c_str(), "S %c %c %f", &fxID, &paramID, &value) == 3) {
-        Effect* fx = chain.getEffect((EffectID)fxID);
-        if (!fx) { Serial.println("ERR: Invalid EffectID"); return; }
-        fx->setParam((ParamID)paramID, value);
-        Serial.println("OK");
-    } else if (sscanf(s.c_str(), "B %c %c", &fxID, &bypass) == 2) {
-        Effect* fx = chain.getEffect((EffectID)fxID);
-        if (!fx) { Serial.println("ERR: Invalid EffectID"); return; }
-        fx->setBypass(bypass);
-        Serial.println("OK");
-    } else { Serial.println("ERR: Parse"); }
-}
+// void processCommand(const String& s, AudioChain& chain) {
+//     uint8_t fxID, paramID, bypass;
+//     float value;
 
+//     // Command structure: [Type][EffectID][ParamID/Bypass][Value], ex:
+//     // S 1 2 0.75
+//     // B 1 1
+//     if (sscanf(s.c_str(), "S %c %c %f", &fxID, &paramID, &value) == 3) {
+//         Effect* fx = chain.getEffect((EffectID)fxID);
+//         if (!fx) { Serial.println("ERR: Invalid EffectID"); return; }
+//         fx->setParam((ParamID)paramID, value);
+//         Serial.println("OK");
+//     } else if (sscanf(s.c_str(), "B %c %c", &fxID, &bypass) == 2) {
+//         Effect* fx = chain.getEffect((EffectID)fxID);
+//         if (!fx) { Serial.println("ERR: Invalid EffectID"); return; }
+//         fx->setBypass(bypass);
+//         Serial.println("OK");
+//     } else { Serial.println("ERR: Parse"); }
+// }
+
+// void processCommand(Command& cmd, AudioChain& chain) {
+//     switch (cmd.cmd) {
+//         case 0: // Add Effect
+//     }
+// }
 void setup() {
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial1.begin(115200); // UART1 RX only (pin 0)
 
     while (!Serial && millis() < 4000) {}
 
@@ -91,19 +109,41 @@ void setup() {
     AudioInterrupts();
 }
 
-void loop() { 
-    // Send parameter updates via serial
-    static String cmd;
-    while (Serial.available()) {
-        char c = Serial.read();
-        if (c == '\n' || c == '\r') {
-            if (cmd.length() > 0) { 
-                processCommand(cmd, *chain); 
-                cmd = ""; 
-            }
-        } else if (c == '\b' || c == 127) { if (cmd.length() > 0) cmd.remove(cmd.length() - 1); // backspace or DEL
-        } else { cmd += c; }
+void loop() {
+    if (Serial1.available()) {
+        Command rcvCmd;
+        size_t rcvBytes = 0;
+
+        uint8_t* buf = (uint8_t*)&rcvCmd;
+        Serial1.readBytes((char*)buf, sizeof(Command));
+
+        
+        
+        if (rcvBytes == sizeof(Command)) {
+            Serial.print("\n=== Received Command ===\n");
+            Serial.printf("cmd: %c | ", rcvCmd.cmd);
+            Serial.printf("id1: %c | ", rcvCmd.id1);
+            Serial.printf("id2: %c | ", rcvCmd.id2);
+            Serial.printf("value: %.3f\n", rcvCmd.value);
+            Serial.println("===================");
+            
+            // processCommand(rcvCmd, *chain);
+        } else {
+            Serial.println("ERR: Incomplete Command");
+        }
     }
+    // TESTING: Send parameter updates via serial
+    // static String cmd;
+    // while (Serial.available()) {
+    //     char c = Serial.read();
+    //     if (c == '\n' || c == '\r') {
+    //         if (cmd.length() > 0) { 
+    //             processCommand(cmd, *chain); 
+    //             cmd = ""; 
+    //         }
+    //     } else if (c == '\b' || c == 127) { if (cmd.length() > 0) cmd.remove(cmd.length() - 1); // backspace or DEL
+    //     } else { cmd += c; }
+    // }
 
     if (millis() - logTime >= 1000) {
         logTime = millis();
